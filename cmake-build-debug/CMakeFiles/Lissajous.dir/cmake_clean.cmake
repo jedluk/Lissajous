2cb6fa1a0deafbe430087a361dfc10c3135588a4
jlukasiu <jedrzej.lukasiuk@gmail.com>
2018-02-15 22:57:57 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Lissajous.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lissajous.dir/main.cpp.obj"
+  "Lissajous.pdb"
+  "Lissajous.exe"
+  "Lissajous.exe.manifest"
+  "libLissajous.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lissajous.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
